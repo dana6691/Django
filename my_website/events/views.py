51aa90f1bb37ad1f83cfa3684aabf1452a7456a9
dana6691 <dahee.kim@mnsu.edu>
2022-04-21 15:42:45 -0500

    ADD CRUD
    Signed-off-by: dana6691 <dahee.kim@mnsu.edu>

--- my_website/events/views.py
+++ my_website/events/views.py
@@ -1,7 +1,113 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
+from django.http import HttpResponseRedirect
+from .models import Event, Venue
+from .forms import VenueForm, EventForm
+
+# Delete an Venue
+def delete_venue(request, venue_id):
+    venue = Venue.objects.get(pk=venue_id)
+    venue.delete()
+    return redirect('list-venues')
+
+# Delete an Event
+def delete_event(request, event_id):
+    event = Event.objects.get(pk=event_id)
+    event.delete()
+    return redirect('list-events')
+  
+def update_event(request, event_id):
+    event = Event.objects.get(pk=event_id)
+    form = EventForm(request.POST or None, instance=event)
+    if form.is_valid():
+        form.save()
+        return redirect('list-events')
+
+    return render(request,'events/update_event.html',{
+        'event':event,
+        'form':form
+    })
+
+
+def add_event(request):
+    submitted = False
+    if request.method == "POST":
+        form =  EventForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/add_event?submitted=True')
+    else:
+        form = EventForm
+        if 'submitted' in request.GET:
+            submitted = True
+
+    return render(request,'events/add_event.html',{
+        'form':form,
+        'submitted':submitted
+    })
+
+def update_venue(request, venue_id):
+    venue = Venue.objects.get(pk=venue_id)
+    form = VenueForm(request.POST or None, instance=venue)
+    if form.is_valid():
+        form.save()
+        return redirect('list-venues')
+
+    return render(request,'events/update_venue.html',{
+        'venue':venue,
+        'form':form
+    })
+
+def search_venues(request):
+    if request.method == "POST":
+        searched = request.POST['searched']
+        venues = Venue.objects.filter(name__contains=searched)
+        return render(request,'events/search_venues.html',{  
+            'searched':searched,
+            'venues':venues
+        })
+    else:
+        return render(request,'events/search_venues.html',{  
+        })
+
+
+def show_venue(request, venue_id):
+    venue = Venue.objects.get(pk=venue_id)
+    return render(request,'events/show_venue.html',{
+        'venue':venue
+    })
+
+def list_venues(request):
+    venue_list = Venue.objects.all().order_by('name') # grab information from models.py
+    return render(request,'events/venue.html',{
+        'venue_list':venue_list
+    })
+
+def add_venue(request):
+    submitted = False
+    if request.method == "POST":
+        form =  VenueForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/add_venue?submitted=True')
+    else:
+        form = VenueForm
+        if 'submitted' in request.GET:
+            submitted = True
+
+    return render(request,'events/add_venue.html',{
+        'form':form,
+        'submitted':submitted
+    })
+
+def all_events(request):
+    event_list = Event.objects.all().order_by('event_date','name') # grab all information from models.py
+    return render(request,'events/event_list.html',{
+        'event_list':event_list
+    })
+
 
 def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
     name = "John"
