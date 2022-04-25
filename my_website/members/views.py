4db88634afc9ec1c2c667df0ae94eb8d3d53f3c6
dana6691 <dahee.kim@mnsu.edu>
2022-04-24 20:05:20 -0500

    register users
    Signed-off-by: dana6691 <dahee.kim@mnsu.edu>

--- my_website/members/views.py
+++ my_website/members/views.py
@@ -2,7 +2,7 @@ from urllib.robotparser import RequestRate
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 
 def logout_user(request):
     logout(request)
@@ -27,5 +27,17 @@ def login_user(request):
 
 def register_user(request):
     if request.method == "POST":
-        forms = 
-    return render(request,'authenticate/register_user.html')
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request,("Registration Sucessful."))
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+        return render(request, 'authenticate/register_user.html', {
+            'form':form,
+    })
